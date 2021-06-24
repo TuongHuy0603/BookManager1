960040e05556b93ec587d30678b9d3bb15ea63ac
TuongHuy0603 <eresh38111@gmail.com>
2021-06-24 16:44:42 +0700

    Initial commit

---
+++ WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        BookManagerContext context = new BookManagerContext();
+        //1. Lấy tất toàn bộ thông tin sách từ bảng Book
+
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            return View(listBook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [Authorize]
+        public ActionResult CreateBook([Bind(Include = "ID,Title,Description,Author,Images,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Books.Add(book);
+                context.SaveChanges();
+                return RedirectToAction("ListBook");
+            }
+
+            return View(book);
+        }
+        [Authorize]
+        public ActionResult EditBook([Bind(Include = "ID,Title,Description,Author,Images,Price")] Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Books.AddOrUpdate(book);
+                context.SaveChanges();
+                return RedirectToAction("ListBook");
+            }
+
+            return View(book);
+        }
+        [Authorize]
+        public ActionResult DeleteBook(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Book book =  context.Books.Find(id);
+            if (book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [HttpPost, ActionName("DeleteBook")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Book book = context.Books.Find(id);
+            context.Books.Remove(book);
+            context.SaveChanges();
+            return RedirectToAction("ListBook");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                context.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+       
+    }
+    }
---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,35 @@
+﻿namespace WebApplication1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required(ErrorMessage = "Id không được để trống")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Tiêu đề không được quá 100 ký tự")]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+     
+        [Required(ErrorMessage = "Mô tả không được để trống")]
+        [StringLength(250)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Tác giả không được quá 30 ký tự")]
+        [StringLength(30)]
+        public string Author { get; set; }
+
+      
+        public string Images { get; set; }
+ 
+        [Range(1000, 1000000, ErrorMessage = "Giá Từ 1000 - 1000000")]
+        public int Price { get; set; }
+    }
+}
